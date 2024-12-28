62d72fd22528bb1f70d2bc2b2bd8b41e83874d9c
Douglas Reis <douglas.vieira@outlook.com>
2024-12-27 23:15:56 -0300

    ajustes...

--- api/app.py
+++ api/app.py
@@ -4,12 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuração do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///evento_futebol.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///partidas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Modelos do banco de dados
-class Evento(db.Model):
+class Partida(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10), nullable=False)
     hora = db.Column(db.String(5), nullable=False)
@@ -23,17 +23,13 @@ class Jogador(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     posicao = db.Column(db.String(10), nullable=False)
     status = db.Column(db.String(10), nullable=False)  # 'principais', 'goleiros', 'espera'
-    evento_id = db.Column(db.Integer, db.ForeignKey('evento.id'), nullable=False)
-
-
+    partida_id = db.Column(db.Integer, db.ForeignKey('partida.id'), nullable=False)
 
 @app.after_request
 def aplicar_codificacao(response):
     response.headers['Content-Type'] = 'application/json; charset=utf-8'
     return response
 
-
-# Substituir o decorador @app.before_first_request
 @app.before_request
 def inicializar_banco():
     if not hasattr(app, 'db_initialized'):
@@ -43,12 +39,12 @@ def inicializar_banco():
 
 
 # Endpoints da API
-@app.route('/evento', methods=['POST'])
-def criar_evento_api():
+@app.route('/partida', methods=['POST'])
+def criar_partida_api():
     dados = request.json
 
-    # Criar o novo evento
-    novo_evento = Evento(
+    # Criar o novo partida
+    nova_partida = Partida(
         data=dados['data'],
         hora=dados['hora'],
         local=dados['local'],
@@ -56,7 +52,7 @@ def criar_evento_api():
         slots_goleiros=dados['slots_goleiros'],
         slots_espera=dados['slots_espera']
     )
-    db.session.add(novo_evento)
+    db.session.add(nova_partida)
     db.session.commit()
 
     # Adicionar jogadores, se fornecidos
@@ -65,28 +61,28 @@ def criar_evento_api():
         posicao = jogador_data.get('posicao', 'jogador')
         status = 'principais' if posicao == 'jogador' else 'goleiros'
 
-        if status == 'principais' and Jogador.query.filter_by(evento_id=novo_evento.id, status='principais').count() < novo_evento.slots_primarios:
-            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='principais', evento_id=novo_evento.id)
-        elif status == 'goleiros' and Jogador.query.filter_by(evento_id=novo_evento.id, status='goleiros').count() < novo_evento.slots_goleiros:
-            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='goleiros', evento_id=novo_evento.id)
-        elif Jogador.query.filter_by(evento_id=novo_evento.id, status='espera').count() < novo_evento.slots_espera:
-            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='espera', evento_id=novo_evento.id)
+        if status == 'principais' and Jogador.query.filter_by(partida_id=nova_partida.id, status='principais').count() < nova_partida.slots_primarios:
+            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='principais', partida_id=nova_partida.id)
+        elif status == 'goleiros' and Jogador.query.filter_by(partida_id=nova_partida.id, status='goleiros').count() < nova_partida.slots_goleiros:
+            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='goleiros', partida_id=nova_partida.id)
+        elif Jogador.query.filter_by(partida_id=nova_partida.id, status='espera').count() < nova_partida.slots_espera:
+            jogador = Jogador(nome=jogador_data['nome'], posicao=posicao, status='espera', partida_id=nova_partida.id)
         else:
             return jsonify({"status": "erro", "mensagem": f"Não foi possível adicionar o jogador {jogador_data['nome']} devido à falta de slots disponíveis."})
 
         db.session.add(jogador)
 
     db.session.commit()
-    return jsonify({"status": "sucesso", "mensagem": "Evento e jogadores criados com sucesso.", "evento_id": novo_evento.id})
+    return jsonify({"status": "sucesso", "mensagem": "partida e jogadores criados com sucesso.", "partida_id": nova_partida.id})
 
 
-@app.route('/evento/<int:evento_id>', methods=['GET'])
-def mostrar_evento_api(evento_id):
-    evento = Evento.query.get(evento_id)
-    if not evento:
-        return jsonify({"status": "erro", "mensagem": "Evento não encontrado."})
+@app.route('/partida/<int:partida_id>', methods=['GET'])
+def mostrar_partida_api(partida_id):
+    partida = Partida.query.get(partida_id)
+    if not partida:
+        return jsonify({"status": "erro", "mensagem": "partida não encontrado."})
 
-    jogadores = Jogador.query.filter_by(evento_id=evento.id).all()
+    jogadores = Jogador.query.filter_by(partida_id=partida.id).all()
     jogadores_dict = {
         "principais": [j.nome for j in jogadores if j.status == 'principais'],
         "goleiros": [j.nome for j in jogadores if j.status == 'goleiros'],
@@ -94,9 +90,9 @@ def mostrar_evento_api(evento_id):
     }
 
     return jsonify({
-        "data": evento.data,
-        "hora": evento.hora,
-        "local": evento.local,
+        "data": partida.data,
+        "hora": partida.hora,
+        "local": partida.local,
         "jogadores": jogadores_dict
     })
 
@@ -104,22 +100,22 @@ def mostrar_evento_api(evento_id):
 @app.route('/jogador', methods=['POST'])
 def adicionar_jogador_api():
     dados = request.json
-    evento_id = dados.get('evento_id')  # Agora esperamos o evento_id no corpo da requisição
+    partida_id = dados.get('partida_id')  # Agora esperamos o partida_id no corpo da requisição
     
-    # Buscar o evento pelo ID
-    evento = Evento.query.get(evento_id)
-    if not evento:
-        return jsonify({"status": "erro", "mensagem": "Evento não encontrado."})
+    # Buscar o partida pelo ID
+    partida = Partida.query.get(partida_id)
+    if not partida:
+        return jsonify({"status": "erro", "mensagem": "partida não encontrado."})
 
     posicao = dados.get('posicao', 'jogador')
     status = 'principais' if posicao == 'jogador' else 'goleiros'
 
-    if status == 'principais' and Jogador.query.filter_by(evento_id=evento.id, status='principais').count() < evento.slots_primarios:
-        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='principais', evento_id=evento.id)
-    elif status == 'goleiros' and Jogador.query.filter_by(evento_id=evento.id, status='goleiros').count() < evento.slots_goleiros:
-        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='goleiros', evento_id=evento.id)
-    elif Jogador.query.filter_by(evento_id=evento.id, status='espera').count() < evento.slots_espera:
-        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='espera', evento_id=evento.id)
+    if status == 'principais' and Jogador.query.filter_by(partida_id=partida.id, status='principais').count() < partida.slots_primarios:
+        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='principais', partida_id=partida.id)
+    elif status == 'goleiros' and Jogador.query.filter_by(partida_id=partida.id, status='goleiros').count() < partida.slots_goleiros:
+        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='goleiros', partida_id=partida.id)
+    elif Jogador.query.filter_by(partida_id=partida.id, status='espera').count() < partida.slots_espera:
+        jogador = Jogador(nome=dados['nome'], posicao=posicao, status='espera', partida_id=partida.id)
     else:
         return jsonify({"status": "erro", "mensagem": "Todos os slots estão cheios."})
 
@@ -132,21 +128,21 @@ def adicionar_jogador_api():
 def remover_jogador_api():
     dados = request.json
     nome_jogador = dados.get('nome')
-    evento_id = dados.get('evento_id')  # Esperando o evento_id na requisição
+    partida_id = dados.get('partida_id')  # Esperando o partida_id na requisição
     
-    # Buscar o evento pelo ID
-    evento = Evento.query.get(evento_id)
-    if not evento:
-        return jsonify({"status": "erro", "mensagem": "Evento não encontrado."})
+    # Buscar o partida pelo ID
+    partida = partida.query.get(partida_id)
+    if not partida:
+        return jsonify({"status": "erro", "mensagem": "partida não encontrado."})
     
-    # Buscar o jogador pelo nome e evento_id
-    jogador = Jogador.query.filter_by(nome=nome_jogador, evento_id=evento.id).first()
+    # Buscar o jogador pelo nome e partida_id
+    jogador = Jogador.query.filter_by(nome=nome_jogador, partida_id=partida.id).first()
     if not jogador:
-        return jsonify({"status": "erro", "mensagem": f"{nome_jogador} não encontrado no evento."})
+        return jsonify({"status": "erro", "mensagem": f"{nome_jogador} não encontrado no partida."})
 
     db.session.delete(jogador)
     db.session.commit()
-    return jsonify({"status": "sucesso", "mensagem": f"{nome_jogador} removido do evento."})
+    return jsonify({"status": "sucesso", "mensagem": f"{nome_jogador} removido do partida."})
 
 
 if __name__ == "__main__":
